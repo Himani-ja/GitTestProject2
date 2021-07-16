a6c271628edeab0464c7b237a478d1828751fb73
Himani-ja <himani.jain2309@gmail.com>
2021-07-16 18:58:49 +0530

    First commit

---
+++ GitTestProject2/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,11 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String  code = "SE";
+        if(code=="SE") 
+        	System.out.println("SE");}
+
+}
